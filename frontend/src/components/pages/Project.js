d7211c3bb705b6eda91036ed021e5fb46d36014e
Henrique Navarro Morais <henriquenavarromorais@gmail.com>
2023-04-24 12:41:56 -0300

    commit

--- frontend/src/components/pages/Project.js
+++ frontend/src/components/pages/Project.js
@@ -1,14 +1,20 @@
-import { useParams } from "react-router-dom";
+import { Form, useParams } from "react-router-dom";
 import styles from "./Project.module.css";
 import { useEffect, useState } from "react";
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
-import ProjectForm from "../project/ProjectForm"
+import ProjectForm from "../project/ProjectForm";
+import Message from "../layout/Message";
+import ServiceForm from "../service/ServiceForm";
+import { parse, v4 as uuidv4 } from "uuid";
 
 const Project = () => {
   const { id } = useParams();
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
 
   useEffect(() => {
     setTimeout(() => {
@@ -29,8 +35,46 @@ const Project = () => {
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
-  function editPost() {
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
+  }
+  function createService(project) {
+    const lastService = project.services[project.services.length - 1];
+    lastService.id = uuidv4();
+    const lastServiceCusto = lastService.custo;
+    const newCusto = parseFloat(project.custo) + parseFloat(lastServiceCusto);
 
+    if (newCusto > parseFloat(project.orcamento)) {
+      setMessage("Orcamento ultrapassado, verifique o valor do Serviço");
+      setType("error");
+      project.services.pop();
+      return false;
+    }
+  }
+
+  function editPost(project) {
+    setMessage("");
+
+    if (project.orcamento < project.custo) {
+      setMessage("O orcamento não pode ser menor que o custo!");
+      setType("error");
+      return false;
+    }
+    fetch(`http://localhost:8080/projects/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage("Projeto atualizado com sucesso!");
+        setType("sucess");
+      })
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -38,6 +82,7 @@ const Project = () => {
       {project.name ? (
         <div className={styles.project_details}>
           <Container customClass="column">
+            {message && <Message type={type} msg={message} />}
             <div className={styles.details_container}>
               <h1>Projeto: {project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
@@ -65,6 +110,25 @@ const Project = () => {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar Serviço" : "Fechar"}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && (
+                  <ServiceForm
+                    handleSubmit={createService}
+                    btnText="Adicionar Serviço"
+                    projectData={project}
+                  />
+                )}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+              <p>Itens de Serviços</p>
+            </Container>
           </Container>
         </div>
       ) : (
---
+++ frontend/src/components/service/ServiceForm.js
@@ -0,0 +1,46 @@
+import styles from "../project/ProjectForm.module.css";
+import Input from "../form/Input";
+import SubmitButton from "../form/SubmitButton";
+import { useState } from "react";
+
+const ServiceForm = ({ handleSubmit, btnText, projectData }) => {
+  const [service, setService] = useState({});
+
+  function submit(e) {
+    e.preventDefault();
+    projectData.services.push(service);
+    handleSubmit(projectData);
+  }
+  function handleChange(e) {
+    setService({ ...service, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <form onSubmit={submit} className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do Serviço"
+        name="name"
+        placeholder="Insira o nome do Serviço"
+        handleOnChange={handleChange}
+      />
+      <Input
+        type="number"
+        text="Custo do Serviço"
+        name="custo"
+        placeholder="Insira o valor total"
+        handleOnChange={handleChange}
+      />
+      <Input
+        type="text"
+        text="Descrição do Serviço"
+        name="descricao"
+        placeholder="Descreva o Serviço"
+        handleOnChange={handleChange}
+      />
+      <SubmitButton text={btnText} />
+    </form>
+  );
+};
+
+export default ServiceForm;
